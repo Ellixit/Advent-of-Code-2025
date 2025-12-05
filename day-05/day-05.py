2c28d22d2bfcc14c61771c5437fd457973d3e8b9
Ellixit <jeffrey.xiao03@gmail.com>
2025-12-05 09:42:54 -0500

    day 05, part 2

--- day-05/day-05.py
+++ day-05/day-05.py
@@ -1,35 +1,41 @@
 
 def find_fresh_ingredients():
     
-    fresh_ingredients = 0
-    
     with open("day-05-input.txt", "r") as file:
         
         lines = [line.strip() for line in file.readlines()]
         
         ranges      = []
-        scan_ranges = True
         
         for line in lines:
-            
             if line == "":
-                scan_ranges = False
-                continue
-                
-            if scan_ranges:
-                line_parts = line.split("-")
-                ranges.append((int(line_parts[0]), int(line_parts[1])))
-            else:
-                fresh_ingredients += check_ranges(ranges, int(line))
+                break
+            line_parts = line.split("-")
+            ranges.append((int(line_parts[0]), int(line_parts[1])))
+            
+        ranges = merge_ranges(ranges)
+
+        fresh_ingredients = count_ranges(ranges)
 
     return fresh_ingredients
 
-def check_ranges(ranges, number):
+def merge_ranges(ranges):
+    ranges = sorted(ranges, key=lambda x: x[0])
+    merged = []
+    
+    for start, end in ranges:
+        if not merged or start > merged[-1][1]:
+            merged.append([start, end])
+        else:
+            merged[-1][1] = max(merged[-1][1], end)
+    return merged
+
+def count_ranges(ranges):
+    total = 0
     
     for range_tuple in ranges:
-        if number > range_tuple[0] and number <= range_tuple[1]:
-            return 1
+        total += range_tuple[1] - range_tuple[0] + 1
         
-    return 0
+    return total
 
 print(find_fresh_ingredients())
