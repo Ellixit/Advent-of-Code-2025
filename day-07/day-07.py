edf0815ebf24e7e0e4466fcaf6bf58fa0461617a
Ellixit <jeffrey.xiao03@gmail.com>
2025-12-09 09:43:33 -0500

    day 07, part 2

--- day-07/day-07.py
+++ day-07/day-07.py
@@ -45,4 +45,42 @@ def calculate_beam_split():
     print(splits)
 
 
-calculate_beam_split()
+def find_timelines():
+
+    with open("day-07-input.txt", "r") as file:
+        rows = [list(row.rstrip("\n")) for row in file]
+
+    start_index = rows[0].index("S")
+    memo = {}
+
+    timelines = find_timelines_helper(1, start_index, rows, memo)
+    print(timelines)
+
+
+def find_timelines_helper(row_idx, beam_idx, rows, memo):
+    
+    # Beam out of bounds
+    if beam_idx < 0 or beam_idx >= len(rows[0]):
+        return 0
+
+    # Reached bottom of graph
+    if row_idx == len(rows):
+        return 1
+
+    # Check memo for previously calculated path
+    key = (row_idx, beam_idx)
+    if key in memo:
+        return memo[key]
+
+    if rows[row_idx][beam_idx] == "^":
+        val = (
+            find_timelines_helper(row_idx+1, beam_idx-1, rows, memo) +
+            find_timelines_helper(row_idx+1, beam_idx+1, rows, memo)
+        )
+    else:
+        val = find_timelines_helper(row_idx+1, beam_idx, rows, memo)
+
+    memo[key] = val
+    return val
+
+find_timelines()
